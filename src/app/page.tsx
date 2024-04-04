e382b2775cd912e7ef2bc9257326c351452b9e13
Ulises <ulisescappa@gmail.com>
2024-04-04 20:43:12 -0300

    Add blackhole video.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Hero from "@/components/main/Hero";
 
 export default function Home() {
   return (
-    <main>
-      Portfolio
+    <main className="h-full w-full">
+      <div className="flex flex-col h-[850px] gap-20">
+        <Hero />
+      </div>
     </main>
   );
 }
